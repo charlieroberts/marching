99c365ad1eba7eddb588ca55fa2a7f22814a4f58
charlieroberts <charlie@charlie-roberts.com>
2018-12-07 10:53:39 -0500

    demo changes

--- playground/playground.bundle.js
+++ playground/playground.bundle.js
@@ -12268,40 +12268,25 @@ march(
 .render()`
 
 },{}],17:[function(require,module,exports){
-module.exports = `mat1 = Material( 'phong', Vec3(.05),Vec3(4,0,2),Vec3(1), 64, Vec3(2,2,.25) )
+module.exports = `mat1 = Material( 'phong', Vec3(.0),Vec3(.5,0,0),Vec3(1), 32, Vec3(0) )
  
 march(
   ry = Rotation(
-    rx = Rotation(
-      Intersection(
-        Box( Vec3(1.5) ),
-        m = Mandelbulb(.5, Vec3(0), mat1 )
-      ),
-      Vec3(0,1,0)
+    Intersection(
+      Box( Vec3(1) ),
+      m = Mandelbulb(.5, Vec3(0), mat1 )
     ),
-    Vec3(1,0,0)
+    Vec3(1,0,0),
+    Math.PI / 2.01
   )   
 )
 .light( 
-  Light( Vec3(0), Vec3(.5), .5 )
+  Light( Vec3(0,0,4), Vec3(1), .25 )
 )
-.fog( 1, Vec3(0) )
-.background( Vec3(0) )
-.render( 3, true )
+.render(3, true)
 .camera( 0,0,3 )
  
-x = 0
-y = 0
-window.onmousemove = e => {
-  x = e.clientX / window.innerWidth
-  y = e.clientY / window.innerHeight
-}
- 
-callbacks[1] = time => {
-  ry.angle = y * 6.28
-  rx.angle = x * 6.28
-  m.a = 2 + time % 14
-}`
+callbacks[1] = t => m.a = 7 + Math.sin( t / 4 ) * 4`
 
 },{}],18:[function(require,module,exports){
 module.exports = `// because, like, marching.js, snare drums, marching...

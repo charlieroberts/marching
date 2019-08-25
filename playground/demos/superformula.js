2450c5a6b1cd37a740f6fe364e59fc736d34501f
charlieroberts <charlie@charlie-roberts.com>
2019-08-25 03:40:07 -0400

    added texturing to superformula

--- playground/demos/superformula.js
+++ playground/demos/superformula.js
@@ -1,38 +1,35 @@
-module.exports = `mat1 = Material( 'phong', Vec3(.0),Vec3(0),Vec3(1), 16, Vec3(0,.25,4) )
+module.exports = `// a 3D superformula essentially two 2D supershapes,
+// first six coefficients govern one, second
+// six coefficients govern the other.
+
+mat1 = Material( 'phong', Vec3(.0),Vec3(0),Vec3(1), 16, Vec3(0,.25,4) )
  
 m = march(
- 
-  // a 3D superformula essentially two 2D supershapes,
-  // first six coefficients govern one, second
-  // six coefficients govern the other. In this example
-  // the two supershapes are the same.
-
   s = SuperFormula(
     1, 1, 16, 1, 1, 1,
     1, 1, 16, 1, 1, 1
   )
-  .translate( 0, .5, 0 )
-  .rotate( 0, 0,1,0 )
-  .material( mat1 ),
-
+  .translate( 0, .5, .85 )
+  .material( mat1 )
+  .texture( 'truchet', { color:.125, scale:30 } ),
+ 
   Plane( Vec3(0,1,0), 1 ).material( Material('phong', Vec3(.15), Vec3(1) ) )
 )
 .light( 
   Light( Vec3(0,5,0), Vec3(.25,.25,.5), .5 ),
   Light( Vec3(3,3,0), Vec3(.5), .5 )
 )
-.fog( .1, Vec3(0) )
-.shadow(16)
+.fog( .25, Vec3(0) )
 .render( 2, true )
-.camera( 0,0,4 )
-
+.camera( 0,0,5 )
+ 
 onframe = time => {
   t = 12
   s.n1_1 = Math.PI + Math.sin( time )
   s.n1_2 = Math.PI + Math.cos( time )
   s.m_1 = Math.sin( time / 2 ) * t
   s.m_2 = Math.cos( time / 2 ) * t
-  s.rotate( time * 10 )
+  s.rotate( time * 10, 0,1,0 )
 }
 
 // thanks to https://github.com/Softwave/glsl-superformula`

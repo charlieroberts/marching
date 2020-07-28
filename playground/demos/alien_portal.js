a1241a74e1c9ba275e0acbd84db09c13117f15ce
charlieroberts <charlie@charlie-roberts.com>
2020-07-28 15:49:12 -0700

    over-the-top with post-processing

--- playground/demos/alien_portal.js
+++ playground/demos/alien_portal.js
@@ -1,23 +1,25 @@
-module.exports = `mat1 = Material( 'phong', Vec3(.05), Vec3(1), Vec3(3), 64, Vec3( 0,4,4 ) )
+module.exports = `mat = Material( 'phong', Vec3(.05), Vec3(1), Vec3(3), 64, Vec3( 0,6,4) )
  
 m = march(
   StairsUnion(
+    Plane( Vec3(0,.5,0) ).material( mat ),
     PolarRepeat(
       PolarRepeat(
-        Torus82().material( mat1 ),
+        Torus82().material( mat ),
         20,
         2.75
       ).rotate(90, 1,0,0 ),
       25,
       2
     ),
-    Plane( Vec3(0,.5,0) )
-      .material( mat1 )
-      .texture('noise', { strength:.15, scale:20 }),
     .25
-  )
+  ),
+  Plane( Vec3(0,.5,0) )
+    .bump( Texture('noise', { strength:.5, scale:20 }) )
 )
 .fog( .15, Vec3(0) )
-.light( Light( Vec3(0,.65,0), Vec3(1), .25 ) )
+.light( Light( Vec3(0,.25,0), Vec3(1,.5,.25), .125 ) )
+.post( Antialias(2), Bloom(.25,3), Focus(.05, .005 ))
+.resolution(1)
 .render()
 .camera( 0, 0, 10 )`
